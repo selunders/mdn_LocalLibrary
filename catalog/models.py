804cea157d0ec22749063921c77d411a491f95a4
Seth Lunders <sethlunders@gmail.com>
2022-03-24 16:28:54 -0700

    Add languages model

--- catalog/models.py
+++ catalog/models.py
@@ -11,6 +11,14 @@ class Genre(models.Model):
         """String for representing the Model object."""
         return self.name
 
+class Language(models.Model):
+    """Model representing a language."""
+    name = models.CharField(max_length=200, help_text="Enter the book's natural language (e.g. English, Spanish, Japanese, etc.")
+
+    def __str__(self):
+        """String for representing the Lanuage object"""
+        return self.name
+
 
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
@@ -19,7 +27,6 @@ class Book(models.Model):
     # Foreign Key used because book can only have one author, but authors can have multiple books
     # Author as a string rather than object because it hasn't been declared yet in the file
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, unique=True,
                              help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
@@ -28,6 +35,7 @@ class Book(models.Model):
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
 
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
     def __str__(self):
         """String for representing the Model object."""
         return self.title
