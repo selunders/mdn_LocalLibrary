0433bdaf76889bfbbac5338fd7feb06905eab47e
Seth Lunders <sethlunders@gmail.com>
2022-03-30 11:03:10 -0700

    Add page to view books on loan.

--- LocalLibrary/urls.py
+++ LocalLibrary/urls.py
@@ -32,7 +32,6 @@ urlpatterns += [
 ]
 
 #Add Django site authentication urls (for login, logout, password management)
-
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
